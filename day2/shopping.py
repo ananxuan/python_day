deb79e04011ec83c64a45ee606d31083c674d95a
dianshen <710275039@qq.com>
2016-04-17 23:03:00 +0800

    pc

--- day2/shopping.py
+++ day2/shopping.py
@@ -16,11 +16,12 @@ import prettytable
 
 LIST_PRODUCT_MOD = ['id','name','price','count']
 LIST_PRODUCT = []
+LIST_PRODUCT_COPY = []
 LIST_CART = []
 IS_LOGIN = False
-TOTAL_MONEY = 0
 AUTH_FILE = 'user.txt'
 AUTH_TYPE = 'file'
+PAY_TOTAL_MONEY = 0
 
 def login():
 
@@ -32,32 +33,180 @@ def helpme2():
     print("""
         pay     确认订单，进入付款！
         modify  修改购物车
-        quit    退出购物商城
+        quit    返回购物商城继续购物
         """
     )
 
 
-def pay():
-    print("您购买的宝贝如下：")
-    helpme2()
-    print_cart()
-    print("请支付\n")
-    if IS_LOGIN == False:
-        while True:
-            name = input("输入你的账户:> ").strip()
-            passwd = input("输入的密码:> ").strip()
-            auth_url = "%s@%s@%s@%s"%(name,passwd,AUTH_FILE,AUTH_TYPE)
-            if warehouse.auth(auth_url):
-                print("ok")
-            else:
-                print("not auth")
-        pass
+def update_product_file():
+    with open('product.txt','w',encoding='utf-8') as f:
+        m = 0
+        # print("update_product_file:",LIST_PRODUCT)
+        n = len(LIST_PRODUCT)
+        for i in LIST_PRODUCT:
+            str2 = "%s||%s||%s||%s"%(i["id"],i["name"],i["price"],i["count"])
+            f.write(str2)
+            if m < n-1:
+                f.write("\n")
+            m += 1
 
-    return  True
+
+def charge(name,pay_total_money):
+    is_auth = False
+    is_charge = False
+    # print("charge:name=",name)
+    with open(AUTH_FILE,'r') as f:
+        content = f.readlines()
+
+    with open(AUTH_FILE,'w') as f:
+        card_error_num = 0
+        CARD_LOGIN_FLAG = False
+        cash = 0
+        while card_error_num < 3:
+            card_passwd = input("请输入银行卡密码> ").strip()
+            m = 0
+            for i in content:
+                i = i.strip().split("||")
+                # print(i)
+                if name == i[0] and card_passwd == i[3]:
+                    cash = round(float(i[4]),2)
+                    CARD_LOGIN_FLAG = True
+                    break
+                m += 1
+            if CARD_LOGIN_FLAG == True:
+                if cash >= pay_total_money:
+                    # print(i)
+                    temp_cash = round(float(i[4]),2)
+                    # print(temp_cash)
+                    temp_cash -= pay_total_money
+                    # print(temp_cash)
+                    i[4] = str(round(temp_cash,2))
+                    # print(i[4])
+                    str1 = "||".join(i)
+                    content[m] = str1 + "\n"
+                    # print(content[m])
+                    # print(content)
+                    is_charge = True
+                    update_product_file()
+                    break
+                else:
+                    is_charge = False
+                    print("余额不足！")
+                    break
+            else:
+                is_charge = False
+                card_error_num +=1
+                print("银行卡密码错误！")
+
+        if is_charge == True:
+            n = len(content)
+            m = 0
+            for i in content:
+                # print(i)
+                if m == n -1:
+                    i = i.strip()
+                f.write(i)
+                m += 1
+
+    return  is_charge
+
+
+def helpme3():
+    print(
+        """
+        id      输入要修改的商品id
+        ok      确认修改
+        """
+    )
+
+def modify():
+    while True:
+        helpme3()
+        print_cart()
+        modify_input = input("请输入要修改的商品id> ").strip()
+        if modify_input.isdigit():
+            m_p_id = int(modify_input)
+            m = 0
+            for i in LIST_CART:
+                if i[0] == m_p_id:
+                    while True:
+                        m_p_c = int(LIST_PRODUCT[m_p_id]["count"]) + LIST_CART[m][2]
+                        print("%s库存%d件"%(LIST_PRODUCT[m_p_id]["name"],m_p_c))
+                        modify_count_input = input("请输入修改后的件数> ").strip()
+                        if modify_count_input.isdigit():
+                            m_p_count = int(modify_count_input)
+                            if m_p_count <= m_p_c:
+                                LIST_CART[m][2] = m_p_count
+                                # auto_update_product(m_p_id,m_p_count)
+                                LIST_PRODUCT[m_p_id]["count"] = str(m_p_c - m_p_count)
+                                break
+
+                            else:
+                                print("库存不够啊！")
+                        else:
+                            print("输入错误！")
+                    break
+
+            # return True
+        elif "ok" == modify_input:
+            return True
+        else:
+            print("输入错误！")
+
+
+
+def pay():
+    is_pay = False
+    IS_LOGIN = False
+    global PAY_TOTAL_MONEY
+    while True:
+        print("您购买的宝贝如下：")
+        print_cart()
+        helpme2()
+        print("请支付\n")
+        pay_input = input("请输入> ").strip()
+        name = ""
+        if "pay" == pay_input:
+            pay_login_num = 0
+            if IS_LOGIN == False:
+                while pay_login_num < 3:
+                    name = input("输入你的账户:> ").strip()
+                    passwd = input("输入的密码:> ").strip()
+                    auth_url = "%s@%s@%s@%s"%(name,passwd,AUTH_FILE,AUTH_TYPE)
+                    if warehouse.auth(auth_url):
+                        IS_LOGIN = True
+                        break
+                    else:
+                        IS_LOGIN = False
+                        print("not auth")
+                    pay_login_num += 1
+            if IS_LOGIN == True:
+                if charge(name,PAY_TOTAL_MONEY):
+                    is_pay = True
+                    global  LIST_CART
+                    LIST_CART = []
+                    print("支持成功！")
+                    break
+                else:
+                    is_pay = False
+                    print("扣款失败！")
+            else:
+                is_pay = False
+        elif "modify" == pay_input:
+            if modify():
+                pass
+
+        elif "quit" == pay_input:
+            is_pay = False
+            break
+
+
+    return  is_pay
 
 
 def product_list():
-    with open('product.txt','r+',encoding='utf-8') as f:
+    global  LIST_PRODUCT
+    with open('product.txt','r',encoding='utf-8') as f:
         for i in f.readlines():
             i = dict(zip(LIST_PRODUCT_MOD,i.strip().split("||")))
             LIST_PRODUCT.append(i)
@@ -72,25 +221,30 @@ def product_list():
 
 
 def auto_update_product(pid,pcount):
-        LIST_PRODUCT[pid]["count"] = str(int(LIST_PRODUCT[pid]["count"]) - pcount)
+    global  LIST_PRODUCT
+    LIST_PRODUCT[pid]["count"] = str(int(LIST_PRODUCT[pid]["count"]) - pcount)
 
 
 def print_cart():
-    a = prettytable.PrettyTable(["宝贝","购买数量","总金额"])
+    a = prettytable.PrettyTable(["id","宝贝","购买数量","总金额"])
     c = 0
-    TOTAL_MONEY = 0
+    global  PAY_TOTAL_MONEY
+    PAY_TOTAL_MONEY = 0
     for i in LIST_CART:
         # print(i)
         p = round(i[3]*i[2],2)
-        a.add_row([i[1],i[2],p])
+        a.add_row([i[0],i[1],i[2],p])
         c += i[2]
-        TOTAL_MONEY += p
-    a.add_row(["总量",c,TOTAL_MONEY])
+        PAY_TOTAL_MONEY += p
+    a.add_row(["","总量",c,PAY_TOTAL_MONEY])
     print(a)
+    # return TOTAL_MONEY
 
 
 
 def buy():
+    global  LIST_PRODUCT
+    global LIST_CART
     while True:
         helpme1()
         pid = input("请输入> ").strip()
@@ -135,11 +289,14 @@ def buy():
             else:
                 print("输入的id超过范围！")
         elif "check" == pid:
-            if pay():
-                return True
+            # print(LIST_CART)
+            if len(LIST_CART) >0:
+                if pay():
+                    return True
+                else:
+                    return False
             else:
-                pass
-            pass
+                print("购物车位空，不用结算啊！")
         elif "l" == pid:
             print_cart()
         elif "quit" == pid:
@@ -147,7 +304,6 @@ def buy():
             exit()
         else:
             print("输入有误！，请重新输入")
-
 
 
 def helpme1():
@@ -159,24 +315,27 @@ def helpme1():
     """)
 
 
-
 def shop():
     print("欢迎来到DS购物商城！".center(40,'#'))
-    # while True:
+
     if product_list():
         pass
     else:
         return False
-    if buy():
-        pass
-    else:
-        pass
-    # print(LIST_PRODUCT)
+    while True:
+        if buy():
+            pass
+        else:
+            pass
 
 
-if __name__ == '__main__':
+def main():
     if shop():
         pass
     else:
         print("出现不确定错误，程序退出")
         exit()
+
+
+if __name__ == '__main__':
+    main()
--- day2/test.py
+++ day2/test.py
@@ -30,7 +30,38 @@ __author__ = 'DSOWASP'
 # a = float("1.9")
 # # b = a*3
 # print(3*a)
-b ="ok"
-c = "haha"
-a = "%s@%s"%(b,c)
-print(a)
+# b ="ok"
+# c = "haha"
+# a = "%s@%s"%(b,c)
+# print(a)
+# with open("user.txt",'r+') as f:
+#     g =f.readlines()
+#     f.seek(0)
+#     m = 0
+#     for i in g:
+#         i = i.strip().split("||")
+#         if i[0] == "owasp":
+#             temp = float(i[4])
+#             temp -= 100.99
+#             i[4] = str(temp)
+#             str1 = "||".join(i)
+#             g[m] = str1 + "\n"
+#             break
+#         m += 1
+#     for i in g:
+#         f.write(i)
+# content = ['||##username##password##card-id##card-passwd##card-money\n', 'owasp||123456||10133678287||654321||9994.3\n', 'ds||123456||10133678288||654321||10000.00']
+# with open("user.txt","w") as f:
+#     for i in content:
+#         f.write(i)
+# a=[1,"book",30]
+# c=[2,"pc",30]
+# b=[3,"ll",31]
+# import prettytable
+# p = prettytable.PrettyTable(["id","name","count"])
+# p.add_row(a)
+# p.add_row(b)
+# p.add_row(c)
+# print(p)
+if "10.1".isdecimal():
+    print("ok")
