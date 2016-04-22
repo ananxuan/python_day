9d07715e82330bbb9460fab0fd2488809e8babf8
dianshen <710275039@qq.com>
2016-04-22 08:12:40 +0800

    Merge branch 'master' of https://github.com/dianshen/python_day

--- day2/test.py
+++ day2/test.py
@@ -63,5 +63,84 @@ __author__ = 'DSOWASP'
 # p.add_row(b)
 # p.add_row(c)
 # print(p)
-if "10.1".isdecimal():
-    print("ok")
+# if "10.1".isdecimal():
+#     print("ok")
+
+
+# with open('a.txt','a',encoding='utf-8') as f:
+#     f.write("新增的")
+# with open('a.txt','r') as g:
+#     L = g.readlines()
+#     L[2]="change\n"
+#
+#
+# print(L)
+# L = ["sfafa\n","sfafg\n","sfsafag\n"]
+# f = open("a.txt",'w',encoding='utf-8')
+# f.writelines(L)
+# f.close()
+# import collections
+# collections.namedtuple()
+
+# L = ['faf','fhhh','tttt']
+# i = ['ffff']
+# M = []
+# for i in L:
+#     M.append(i)
+#
+# print(M)
+
+'''
+用户信息
+locked.txt
+tenglan
+judy
+king
+
+login.txt
+jack 123456
+bob 123
+dawang 789
+'''
+
+# login_count = 0   #定义登陆次数（初始值为0）
+# user = input("请输入用户名：")
+# if len(user) == 0: #用户名不能为空
+#     print("用户名不能为空，请重新输入")
+# while login_count <3:    #限定登陆次数小于3
+#     is_login = False
+#     with open("locked.txt","r") as locked_file:   #使用with可避免忘记打f.close()
+#         for line in locked_file.readlines():  # 循环遍历每一行
+#             if user == line.strip():      #去除换行符
+#                 print("用户%s已被锁定"%user)
+#                 continue         #跳过后续代码, 继续进行循环
+#     pwd=input("请输入密码：")
+#     with open("login.txt", 'r') as login_file:
+#       for line in login_file.readlines():     #循环遍历每一行
+#         username, passward = line.strip().split()   #去除换行符以空格分割
+#         if username == user and passward == pwd:
+#             # print("success!")
+#             # print("欢迎%s用户成功登录系统" % username)
+#             is_login = True
+#             break
+#             #跳过后续代码, 退出当前循环
+#     if is_login == True:
+#         print("欢迎登陆")
+#         break
+#     else:
+#         print("您的用户名和密码有误，请重新输入！")
+#         login_count += 1                #计数（每登陆一次+1）
+#         # break
+#         #跳过后续代码, 退出当前循环
+# if login_count >=3:
+#         print("您的用户已被锁定")
+#         with open("locked.txt", 'a') as locked:
+#             locked.write("\n"+user)       #密码错误三次写进黑名单
+#
+def bb(x):
+    return x+1
+a = map(bb,[1,2,3,4])
+print(a)
+print(a.__doc__)
+for i in a:
+    print(i)
