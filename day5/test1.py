1f39f8ea0ef88e8eab1c67a41c0b33b9965dcd31
dianshen <710275039@qq.com>
2016-05-10 17:32:49 +0800

    Merge branch 'master' of https://github.com/dianshen/python_day

--- day5/test1.py
+++ day5/test1.py
@@ -9,4 +9,12 @@ for i in range(14):
 
 # print(checkcode)
 
+import 密码复杂度检测 as pc
+
+while True:
+    str1 = input("请输入密码> ").strip()
+    if pc.check_password_complexity(str1,2):
+        print("%s复杂度符合要求"%str1)
+        break
+
 
--- day5/密码复杂度检测.py
+++ day5/密码复杂度检测.py
@@ -1,5 +1,15 @@
 import  re
 '''
+接口调用示例：
+import 密码复杂度检测 as pc
+
+while True:
+    str1 = input("请输入密码> ").strip()
+    if pc.check_password_complexity(str1,2):
+        print("%s复杂度符合要求"%str1)
+        break
+
+
 密码复杂度检查，包括两种复杂度检查，
 level=1时，
     密码不能包含中文，
@@ -8,9 +18,17 @@ level=1时，
     密码必须包含字母、数字、特殊字符
 level-2时，
     包含level=1的条件
-    不能有一个字符连续出现超过字符长度减8次的情况，比如字符长度是10，则不能有一个字符出现2次的情况
+    不能有一个字符连续出现超过字符长度减8次的情况，比如字符长度是10，则不能有一个字符连续出现2次的情况
 '''
 def check_password_complexity(p,level):
+    """
+
+    :param p: 传递进来的待检测的密码
+    :param level: 需要检查的强度
+    :return: 如果密码复杂度符合要求则返回True，否则返回False
+    """
+    # 判断等级是否是1或2
+    assert level in (1,2),'level密码强度检查只能是1或2'
     # 判断是否包含中文
     zhPattern = re.compile(u'[\u4e00-\u9fa5]+')
     if zhPattern.search(p):
@@ -28,7 +46,7 @@ def check_password_complexity(p,level):
     elif level == 1:
         return True
     elif level == 2:
-        # 高级密码复杂度检查，检查字符是否连续出现超过字符串长度减8次
+        # 高级密码复杂度检查，检查字符是否连续出现超过字符串长度减8次,更高级检查待开发
         l = len(p)-8
         # print(l)
         p_temp = p[:9]
@@ -36,21 +54,10 @@ def check_password_complexity(p,level):
             # chars = re.search(r'[%s]{%d,}'%(char,l),p)
             if char*l in p:
                 print("您的密码过于简单")
-                print("%s重复出现超过%d次!"%(char,l))
+                print("%s重复出现超过%d次!重复次数不能超过字符长度减8次"%(char,l))
                 return False
             else:
                 return True
 
-while True:
-    p = input("请输入密码> ").strip()
-
-    if p == 'quit':
-        exit()
-    if check_password_complexity(p,2):
-        flag = 1
-        break
-    else:
-        flag = 0
-
-if flag == 1:
-    print("密码%s符合密码复杂度要求！"%p)
+if __name__ == '__main__':
+    print("请使用\033[1;31;0mheck_password_complexity\033[0m(\033[1;32;0mpassword,level\033[0m)调用此函数")
